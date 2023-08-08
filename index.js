5a5285bc030a068a9b0c05ac75de7027981b55c2
breanna gonzales <115334210+breagonz@users.noreply.github.com>
2023-08-08 14:41:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// ----- Discount Chain -----
+
+// create the logic for their checkout system that returns the total cost of all 
+// refills including if the customer has a subscription and/or a coupon.
+
+
+
+const timmy = {
+    prescription: "acetaminophen",
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+};
+
+const sarah = {
+    prescription: "diphenhydramine",
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+}
+
+const rocky = {
+    prescription: "phenylephrine",
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+}
+
+
+
